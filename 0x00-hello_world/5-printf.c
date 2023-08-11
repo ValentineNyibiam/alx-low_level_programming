bb86a705070554fe52f6dab89fded6778839e295
Valentine Nyibiam <nyibiamsaanmoiyol@gmail.com>
2023-08-11 06:48:58 -0700

    Add script that prints string using the printf function

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+/*
+ * main - Entry point
+ *
+ * printf function
+ *
+ * Return: Always 0 (Sucess)
+ */
+int main (void)
+{
+    printf("with proper grammer, but the outcome is a piece of art,\n");
+    return (0);
+}
