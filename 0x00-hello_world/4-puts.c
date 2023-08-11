17f0e25df44921c6aee1361cde77d02b372b7e6b
Valentine Nyibiam <nyibiamsaanmoiyol@gmail.com>
2023-08-11 05:03:57 -0700

    Add script that prints "Programming is like building a multilingual puzzle", followed by a new line

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+int main()
+{
+     //string initialisation
+    char str[] = "Programming is like building a multilingual puzzle";
+   
+     //writing the string to stdout
+     puts(str); 
+
+     return (0);
+}
