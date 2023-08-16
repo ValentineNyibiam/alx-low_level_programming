e79cdeac2aece4e8de60723d857177d317b62366
Valentine Nyibiam <nyibiamsaanmoiyol@gmail.com>
2023-08-16 05:27:14 -0700

    Update 3-islower.c

--- 0x02-functions_nested_loops/3-islower.c
+++ 0x02-functions_nested_loops/3-islower.c
@@ -1,13 +1,15 @@
 #include "main.h"
 /**
  * _islower - Checks if a character is lower case
- * 
- * return: returns 1 if lower case else 0
+ * @c: the character in ASCII code
+ * Return: 1 if lower case else 0
  */
 int _islower(int c)
 {
+	int c;
+
 	if (c >= 97 && c <= 122)
 		return (1);
-	else 
+	else
 		return (0);
 }
