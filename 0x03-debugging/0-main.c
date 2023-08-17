4e2a8061934e76fb42e38db66203561f71c0aaf0
Valentine Nyibiam <nyibiamsaanmoiyol@gmail.com>
2023-08-17 13:38:39 -0700

    Update function that checks for positive and negative numbers

--- 0x03-debugging/0-main.c
+++ 0x03-debugging/0-main.c
@@ -1,26 +1,16 @@
-#include <stdlib.h>
-#include <time.h>
-#include <stdio.h>
-/* more headers goes there */
+#include "main.h"
 
 /**
- * positive_or_negative - Checks if a number is positive or negative
- * @i: input of integer type
- * Return: 0 (Success)
- */
+* main - tests function that prints if integer is positive or negative
+* Return: 0
+*/
 
-/* betty style doc for function main goes there */
-int positive_or_negative(int i)
+int main(void)
 {
-	int n;
+        int i;
 
-	n = i;
-	/* your code goes there */
-	if (n > 0)
-		printf("%d is positive\n", n);
-	else if (n == 0)
-		printf("%d is zero\n", n);
-	else
-		printf("%d is negative\n", n);
-	return (0);
+        i = 0;
+        positive_or_negative(i);
+
+        return (0);
 }
