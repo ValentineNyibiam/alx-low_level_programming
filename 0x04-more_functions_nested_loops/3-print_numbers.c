1bdd5557b2e3fd623076fad76665ae7113a3f06b
Valentine Nyibiam <nyibiamsaanmoiyol@gmail.com>
2023-08-18 10:15:57 -0700

    Update header file and add function that prints 0 to 9

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,16 @@
+#include "main.h"
+/**
+ * print_numbers - Prints the numbers from 0 to 9
+ *
+ * Return: void
+ */
+void print_numbers(void)
+{
+	int a;
+	
+	for (a = 48; a <= 57; a++)
+	{
+		_putchar(a);
+	}
+	_putchar('\n');
+}
