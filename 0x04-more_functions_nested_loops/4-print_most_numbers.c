d53b4ca3350e0b2f8ffd3dcc1add64aed430f616
Valentine Nyibiam <nyibiamsaanmoiyol@gmail.com>
2023-08-18 12:07:24 -0700

    Update main.h and add a function that prints integers with exception

---
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -0,0 +1,19 @@
+#include "main.h"
+/**
+ * print_numbers - Prints the numbers from 0 to 9
+ *
+ * Return: void
+ */
+void print_most_numbers(void)
+{
+	int a;
+
+	for (a = 48; a <= 57; a++)
+	{
+		if (!(a == 50 || a == 52))
+		{
+			_putchar(a);
+		}
+	}
+	_putchar('\n');
+}
