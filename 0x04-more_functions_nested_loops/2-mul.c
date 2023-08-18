9c993de54c4e33e0eb8d4d5bcb92cbdbd1fcca6b
Valentine Nyibiam <nyibiamsaanmoiyol@gmail.com>
2023-08-18 09:22:50 -0700

    Update my header file and add fuction that finds the product of two integers

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,15 @@
+#include "main.h"
+/**
+ * mul - Finds the product of two integers
+ * @a: First input of integer type
+ * @b: Second input of integer type
+ *
+ * Return: integer
+ */
+int mul(int a, int b)
+{
+	int prod;
+
+	prod = (a *b);
+	return (prod);
+}
