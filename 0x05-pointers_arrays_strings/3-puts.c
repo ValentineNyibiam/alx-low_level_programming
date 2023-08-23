0696dcd41f3d057d1881a57eb7de7c54a7040ff5
Valentine Nyibiam <nyibiamsaanmoiyol@gmail.com>
2023-08-22 17:43:46 -0700

    Update 3-puts.c

--- 0x05-pointers_arrays_strings/3-puts.c
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -1,14 +1,14 @@
 #include "main.h"
 /**
  * _puts - Prints a string to the stdout
- * @s: Input of string type
+ * @str: Input of string type
  * Return: void
  */
 void _puts(char *str)
 {
 	int c;
 
-	for(c = 0; str[c] != '\0'; c++)
+	for (c = 0; str[c] != '\0'; c++)
 		_putchar(str[c]);
 	_putchar('\n');
 }
