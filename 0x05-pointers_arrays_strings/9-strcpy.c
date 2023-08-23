1abbe3ea4e15042c2ea9246356503204623ed839
Valentine Nyibiam <nyibiamsaanmoiyol@gmail.com>
2023-08-22 21:26:35 -0700

    Add a fuction that copies a string from a source to a destination

---
+++ 0x05-pointers_arrays_strings/9-strcpy.c
@@ -0,0 +1,16 @@
+#include "main.h"
+/**
+ * *_stcpy - Copies a string to the buffer
+ * @src: Where string is copied from
+ * @dest: Where copied string is pasted to
+ * Return: dest
+ */
+char *_strcpy(char *dest, char *src)
+{
+	int c;
+
+	for (c = 0; src[c] != '\0'; c++)
+		dest[c] = src[c];
+	dest[c] = '\0';
+	return (dest);
+}
