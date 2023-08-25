69019f80f9ed37259babc0fd46a3aa7c437636d1
Valentine Nyibiam <nyibiamsaanmoiyol@gmail.com>
2023-08-25 07:57:46 -0700

    Update main.h and add a function that converts lowercase to uppercase

---
+++ 0x06-pointers_arrays_strings/5-string_toupper.c
@@ -0,0 +1,20 @@
+#include "main.h"
+/**
+ * *string_toupper - Changes all lowercase letters of a string
+ * to upper
+ * @inp: Input string
+ * Return: char
+ */
+char *string_toupper(char *inp)
+{
+	int i;
+
+	for (i = 0; inp[i] != '\0'; i++)
+	{
+		if (inp[i] > 97 && inp[i] < 90)
+		{
+			inp[i] -= 32;
+		}
+	}
+	return (inp);
+}
