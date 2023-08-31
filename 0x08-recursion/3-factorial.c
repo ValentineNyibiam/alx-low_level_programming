788d2a41824d3f67ba24e06790be10a81e04e16d
Valentine Nyibiam <nyibiamsaanmoiyol@gmail.com>
2023-08-31 16:23:10 -0700

    Update main.h and add function that returns the factorial of a given number

---
+++ 0x08-recursion/3-factorial.c
@@ -0,0 +1,14 @@
+#include "main.h"
+/**
+ * factorial - Returns the factorial of a number
+ * @n: Input of integer type
+ * return: -1 if n is else than one else the factorial of n
+ */
+int factorial(int n)
+{
+	if (n < 0)
+		return(-1);
+	else if (n == 0)
+		return (1);
+	return (n * factorial(n - 1));
+}
