ffadff48e64973a5b863567185270a734386b7f0
Valentine Nyibiam <nyibiamsaanmoiyol@gmail.com>
2023-09-11 13:42:02 -0700

    Update comparison mechanism in 2-print_dog.c

--- 0x0E-structures_typedef/2-print_dog.c
+++ 0x0E-structures_typedef/2-print_dog.c
@@ -11,12 +11,12 @@ void print_dog(struct dog *d)
 {
 	if (d == NULL)
 		return;
-	if (d->name == NULL)
+	if (d->name == 0)
 		printf("(nil)");
 	else
 		printf("Name: %s\n", d->name);
 	printf("Age: %f\n", d->age);
-	if (d->owner == NULL)
+	if (d->owner == 0)
 		printf("(nil)");
 	else
 		printf("Owner: %s\n", d->owner);
