fbd02f7b332a5a8d4c27c575e51a3c4ff8ef09b4
Valentine Nyibiam <nyibiamsaanmoiyol@gmail.com>
2023-09-11 09:09:32 -0700

    Update the struct dog template

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -1,7 +1,16 @@
 #ifndef MAIN_H
 #define MAIN_H
 
-struct dog 
+/**
+ * struct dog - contains dog information
+ * @name: input of string type
+ * @age: input of integer type
+ * @owner: input of string type
+ *
+ * Description: contains the name, age and owner
+ * of a particular dog
+ */
+struct dog
 {
 	char *name;
 	float age;
