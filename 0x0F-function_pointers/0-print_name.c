7fc82038ac54cdef842d4b778cfd69094e4dce65
Valentine Nyibiam <nyibiamsaanmoiyol@gmail.com>
2023-09-13 05:41:34 -0700

    Update 0-print_name.c

--- 0x0F-function_pointers/0-print_name.c
+++ 0x0F-function_pointers/0-print_name.c
@@ -5,12 +5,12 @@
  * print_name - prints a name
  * @name: name to be printed
  * @f: a pointer to a function that prints the name
- * 
+ *
  * Return: void
  */
- void print_name(char *name, void (*f)(char *))
- {
+void print_name(char *name, void (*f)(char *))
+{
 	if (name == NULL || f == NULL)
 		return;
 	f(name);
- }
+}
