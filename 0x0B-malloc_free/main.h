ed5997eba85e033547fc7ffcaf305f780b2bac40
ValentineNyibiam <nyibiamsaanmoiyol@gmail.com>
2023-10-03 16:38:40 +0100

    add header file

---
+++ 0x0B-malloc_free/main.h
@@ -0,0 +1,12 @@
+#ifndef MAIN_H
+#define MAIN_H
+
+/* standard libraries */
+#include <stddef.h>
+#include <stdlib.h>
+
+/* prototypes */
+char *create_array(unsigned int size, char c);
+int _putchar(char c);
+
+#endif /* MAIN_H */
