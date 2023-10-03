d2f164fb2ba57174745c7cd267c1d2bb32aad441
Valentine Nyibiam <nyibiamsaanmoiyol@gmail.com>
2023-10-02 17:37:58 -0700

    add a function that multiplies two integers

---
+++ 0x0A-argc_argv/3-mul.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * main - multiplies two numbers
+ * @argc: number of command lime arguments
+ * @argv: an array of the command line arguments
+ *
+ * Return: 0 (success)
+ */
+int main(int argc, char *argv[])
+{
+	if (argc != 3)
+	{
+		printf("Error\n");
+		return (1);
+	}
+	else
+	{
+		printf("%d\n", atoi(argv[1]) * atoi(argv[2]));
+	}
+	return (0);
+}
