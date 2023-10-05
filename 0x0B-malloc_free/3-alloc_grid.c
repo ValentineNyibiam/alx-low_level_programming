cd993c1d53759ca9f32cf51757e13c1a60452272
ValentineNyibiam <nyibiamsaanmoiyol@gmail.com>
2023-10-05 09:48:59 +0100

    add a fuction that allocates memory for a two dimensional array of integers

---
+++ 0x0B-malloc_free/3-alloc_grid.c
@@ -0,0 +1,41 @@
+#include "main.h"
+
+/**
+ * **alloc_grid - allocates memory for a two dimensional
+ * array of integers
+ * @width: the number of columns of the array
+ * @height: the number of rows in the array
+ *
+ * Return: a double pointer pointing to the
+ * allocated memory for the 2d array of integers
+ */
+int **alloc_grid(int width, int height)
+{
+	int i, j;
+	int **alloc_mem;
+
+	/* check if width or height is less than zero */
+	if (width < 0 || height < 0)
+		return (NULL);
+
+	/* allocate memory for the 2d array */
+	alloc_mem = malloc(sizeof(int) * height);
+
+	/* check if memory allocation is successful */
+	if (alloc_mem == NULL)
+		return (NULL);
+
+	/* intialize each element of the grid to 0 */
+	for (i = 0; i < height; i++)
+	{
+		alloc_mem[i] = malloc(sizeof(int) * width);
+		if (alloc_mem[i] == NULL)
+		{
+			free(alloc_mem[i]);
+			free(alloc_mem);
+		}
+		for (j = 0; j < width; j++)
+			alloc_mem[i][j] = 0;
+	}
+	return (alloc_mem);
+}
