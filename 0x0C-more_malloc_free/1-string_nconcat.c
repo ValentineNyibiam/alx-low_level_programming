c8d6990d0776cb6967021ef729c822211441643c
ValentineNyibiam <nyibiamsaanmoiyol@gmail.com>
2023-10-08 15:02:45 +0100

    update 1-string_nconcat.c

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -13,27 +13,26 @@
 char *string_nconcat(char *s1, char *s2, unsigned int n)
 {
 	char *alloc_mem;
-	unsigned int i, j, str_len1, mem_to_alloc;
+	unsigned int i, j, str_len2, str_len1, mem_to_alloc, n_edge = n;
 
-	/* checks the length of s1 and s2 */
-	str_len1 = _strlen(s1);
+	str_len1 = _strlen(s1); /* checks the length of s1 and s2 */
+	str_len2 = _strlen(s2);
 
-	/* check if NULL passed */
-	if (s1 == NULL)
+	if (s1 == NULL) /* check if NULL passed */
 		s1 = "";
 	if (s2 == NULL)
 		s2 = "";
 
-	/* allocate memory dynamically */
-	mem_to_alloc = str_len1 + n;
-	alloc_mem = malloc((sizeof(char) * mem_to_alloc) + 1);
+	if (n >= str_len2) /* account for edge cases of n */
+		n_edge = str_len2;
 
-	/* check if allocation was successful */
-	if (alloc_mem == NULL)
+	mem_to_alloc = str_len1 + n_edge + 1; /* allocate memory dynamically */
+	alloc_mem = malloc(sizeof(char) * mem_to_alloc);
+
+	if (alloc_mem == NULL) /* check if allocation was successful */
 		return (NULL);
-	printf("%d\n", n);
-	/* initialize allocated memory */
-	for (i = 0, j = 0; i < mem_to_alloc; i++)
+
+	for (i = 0, j = 0; i < mem_to_alloc; i++) /* initialize allocated memory */
 	{
 		if (i < str_len1)
 			alloc_mem[i] = s1[i];
