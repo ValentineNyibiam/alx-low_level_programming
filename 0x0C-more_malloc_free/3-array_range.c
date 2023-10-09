f7bb6f251383e92aa2f9db814a2a8c1d3cace5bf
ValentineNyibiam <nyibiamsaanmoiyol@gmail.com>
2023-10-09 10:59:09 +0100

    add a function that creates an array of integers

--- 0x0C-more_malloc_free/3-array_range.c
+++ 0x0C-more_malloc_free/3-array_range.c
@@ -1,20 +1,37 @@
 #include "main.h"
 
 /**
- * *array_range - creates an array of integers and intializes it with values
- * @min: the minimum integer value which the array will contain
- * @max: the maximum integer value which the array will contain
+ * *array_range - creates an array of integers
+ * @min: the minumum value the array will contain
+ * @max: the maximum value the array will contain
  *
- * Return: a pointer to the allocated array which has been initialized
- * else NULL
+ * Return: a pointer to the newly created array
  */
-int *array_range(int min, int max)
+int *array_arrange(int min, int max)
 {
 	/* initialize variables */
 	int *alloc_mem;
+	int i, mem_alloc;
+
+	/* check if min is greater than max */
+	if (min > max)
+		return (NULL);
+
+	/* determine the amount of memory to allocate */
+	mem_alloc = max - min + 1;
 
 	/* allocate memory dynamically */
-	alloc_mem = 
+	alloc_mem = malloc(sizeof(int) * mem_alloc);
+
 	/* check if allocation was successful */
-	/* intialize allocated memory */
+	if (alloc_mem == NULL)
+		return (NULL);
+
+	/* initalize allocated memory */
+	for (i = 0; i < mem_alloc; i++, min++)
+	{
+		alloc_mem[i] = min;
+	}
+
+	return (alloc_mem);
 }
