0752b0d515a81e2ff09a28bfb3742dcc3abd4205
ValentineNyibiam <nyibiamsaanmoiyol@gmail.com>
2023-10-10 16:44:43 +0100

    add newline character and edit comments in 2-print_dog.c

--- 0x0E-structures_typedef/2-print_dog.c
+++ 0x0E-structures_typedef/2-print_dog.c
@@ -2,7 +2,7 @@
 
 /**
  * print_dog - prints a struct dog
- * @d: a pointer to the struct dog to be printed
+ * @d: struct dog to be printed
  *
  * Return: nothing
  */
@@ -14,5 +14,5 @@ void print_dog(struct dog *d)
 		d->name = "(nil)";
 	if (d->owner == NULL)
 		d->owner = "(nil)";
-	printf("Name: %s \nAge: %f \nOwner: %s", d->name, d->age, d->owner);
+	printf("Name: %s\nAge: %f\nOwner: %s\n", d->name, d->age, d->owner);
 }
