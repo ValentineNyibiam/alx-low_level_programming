1f3b10feaee082001a3d56c3cda915f350f85599
ValentineNyibiam <nyibiamsaanmoiyol@gmail.com>
2023-10-11 13:37:45 +0100

    update header file

--- 0x0F-function_pointers/function_pointers.h
+++ 0x0F-function_pointers/function_pointers.h
@@ -1,10 +1,12 @@
 #ifndef FUNCTION_POINTERS_H
 #define FUNCTION_POINTERS_H
 
-/* prototypes */
-void print_name(char *name, void (*f)(char *));
-
 /* standard libraries */
 #include <stdio.h>
+#include <stdlib.h>
+
+/* prototypes */
+void print_name(char *name, void (*f)(char *));
+void array_iterator(int *array, size_t size, void (*action)(int));
 
 #endif /* FUNCTION_POINTERS_H */
