903c283fbf8772bd153cb1d0063ac5051b0d837a
Valentine Nyibiam <nyibiamsaanmoiyol@gmail.com>
2023-10-12 18:20:19 -0700

    edit betty syle errors

--- 0x10-variadic_functions/1-print_numbers.c
+++ 0x10-variadic_functions/1-print_numbers.c
@@ -2,7 +2,7 @@
 
 /**
  * print_numbers - Prints numbers followed by a new line
- * @seperator: A pointer to the string to be printed
+ * @separator: A pointer to the string to be printed
  * between the printed numbers
  * @n: The number of numbers passed to the function
  * @...: The variable arguments passed to the function
@@ -18,7 +18,7 @@ void print_numbers(const char *separator, const unsigned int n, ...)
 	va_start(args, n);
 	for (i = 0; i < n; i++)
 	{
-		
+
 		printf("%d", va_arg(args, int));
 		if (i == n - 1)
 		{
