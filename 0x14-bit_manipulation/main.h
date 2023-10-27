bea2ab7d8b47cbb022ec08364893869d955969b6
ValentineNybiam <nyibiamsaanmoiyol@gmail.com>
2023-10-27 05:28:44 +0100

    Add header file

---
+++ 0x14-bit_manipulation/main.h
@@ -0,0 +1,19 @@
+#ifndef MAIN_H
+#define MAIN_H
+
+/* Standard libraries */
+#include <stdlib.h>
+#include <stdio.h>
+
+/* Protoypes */
+unsigned int binary_to_uint(const char *b);
+unsigned int _strlen(const char *str);
+void print_binary(unsigned long int n);
+int _putchar(char c);
+int get_bit(unsigned long int n, unsigned int index);
+int set_bit(unsigned long int *n, unsigned int index);
+int clear_bit(unsigned long int *n, unsigned int index);
+unsigned int flip_bits(unsigned long int n, unsigned long int m);
+int get_endianness(void);
+
+#endif /* MAIN_H */
