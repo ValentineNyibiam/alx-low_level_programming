c26658c64d19635ed3ac390e6845a6d984ebb912
ValentineNybiam <nyibiamsaanmoiyol@gmail.com>
2024-02-03 20:12:43 +0100

    Update function description in 6-hash_table_delete.c to meet betty requirements

--- 0x1A-hash_tables/6-hash_table_delete.c
+++ 0x1A-hash_tables/6-hash_table_delete.c
@@ -1,7 +1,7 @@
 #include "hash_tables.h"
 
 /**
- * hash-table_delete - Deletes a hash table
+ * hash_table_delete - Deletes a hash table
  * @ht: The hash table to be deleted
  */
 void hash_table_delete(hash_table_t *ht)
