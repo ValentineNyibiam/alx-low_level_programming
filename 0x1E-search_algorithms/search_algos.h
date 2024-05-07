012cfe7294530dae5fc0e24c5b7e535e3025e7d5
ValentineNyibiam <nyibiamsaanmoiyol@gmail.com>
2024-05-07 17:43:57 +0100

    build(alx-ll-programming:)0x1E-search_algorithms): add header file for 0x1E-search_algorithms directory

---
+++ 0x1E-search_algorithms/search_algos.h
@@ -0,0 +1,11 @@
+#ifndef SEARCH_ALGOS_H
+#define SEARCH_ALGOS_H
+
+/* standard header files */
+#include <stddef.h>
+#include <stdio.h>
+
+/* user defined functions */
+int linear_search(int *array, size_t size, int value);
+
+#endif /* SEARCH_ALGOS_H */
